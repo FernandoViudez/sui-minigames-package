5f64a980fdb1b920dbdc514804864ea64a89738d
Fer_Viudez <fernando.viudez@indelve.com>
2023-04-27 19:08:25 -0300

    Remove provider log

--- run/provider.js
+++ run/provider.js
@@ -1,14 +1,2 @@
-import { JsonRpcProvider, devnetConnection, Connection } from "@mysten/sui.js";
+import { JsonRpcProvider, devnetConnection } from "@mysten/sui.js";
 export const provider = new JsonRpcProvider(devnetConnection);
-
-
-(async () => {
-    console.log(
-        (await provider.getObject({
-            id: '0x13408d0e9e26bb0c4d63004cc3d9209a3ecfcf993c05571cf7b4c7de5768550c',
-            options: {
-                showContent: true
-            }
-        })).data.content.fields.cards
-    )
-})()
